e51828008bc8668f65bce628aaa04ad6a13fe116
Rachelle Fualo <rachellefualo699@gmail.com>
2025-02-14 23:01:36 +0800

    change

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,16 +3,15 @@ import Welcome from '../assets/welcome.png';
 
 function Content() {
   return (
-    <div className="grid grid-cols lg:grid-cols gap-5 ">
-      <article className="flex items-center bg-gray-200 justify-center rounded-xl p-4 sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
-        <div className="">
-          <h1 className="text-3xl pb-2 text-gray-900 dark:text-gray-100">Welcome, Rachelle Fualo!</h1>
-          {/* <h1 className="text-slate-500 font-normal text-sm text-center dark:text-slate-400 ">
-            Let's evaluate your favorite instructors today!
-          </h1> */}
+    <div className="grid grid-cols-1 lg:grid-cols gap-5">
+      <article className="flex flex-wrap items-center bg-gray-200 justify-center rounded-xl p-4 sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <div className="text-center sm:text-left w-full sm:w-auto">
+          <h1 className="text-2xl sm:text-3xl pb-2 text-gray-900 dark:text-gray-100">
+            Welcome, Rachelle Fualo!
+          </h1>
         </div>
-        <div className="flex-shrink-0">
-          <img className="w-[100px] h-auto object-contain" src={Welcome} alt="Welcome" />
+        <div className="flex-shrink-0 mt-4 sm:mt-0 sm:ml-4">
+          <img className="w-[80px] sm:w-[100px] h-auto object-contain" src={Welcome} alt="Welcome" />
         </div>
       </article>
     </div>
